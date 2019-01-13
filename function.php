693c1c89710c80596c4dfbdcd25a1e7ed19ceba2
arinz <arinzeaco@gmail.com>
2019-01-13 16:37:53 +0100

    chart icon

--- function.php
+++ function.php
@@ -1,6 +1,21 @@
 <?php
 include 'config.php';
 
+
+function reArrayFiles($file_post) {
+
+    $file_ary = array();
+    $file_count = count($file_post['name']);
+    $file_keys = array_keys($file_post);
+
+    for ($i=0; $i<$file_count; $i++) {
+        foreach ($file_keys as $key) {
+            $file_ary[$i][$key] = $file_post[$key][$i];
+        }
+    }
+
+    return $file_ary;
+}
 function isUserExists($db,$phone) {
 $stmt = $db->prepare("SELECT * FROM user WHERE phone = :phone");
     $stmt->execute([
